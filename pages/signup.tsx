5bc169354a39e15e2572ce88b982717ac00c80c5
Bradley Brown <bradley@betablazegear.com>
2023-03-30 18:27:02 -0500

    main

---
+++ pages/signup.tsx
@@ -0,0 +1,24 @@
+import type { NextPage } from "next";
+import Layout from "../components/Layout";
+import Section from "../components/Sections/Section";
+import Block from "../components/Blocks/Block";
+import ProjectsTableBlock from "../components/Blocks/ProjectsTableBlock";
+
+import Userfront from "@userfront/react";
+
+const SignUp: NextPage = () => {
+    Userfront.init("vbqxrwgn");
+
+    const SignupForm = Userfront.build({
+        toolId: "dkklbor",
+    });
+
+    return (
+        <div>
+            <h1>Sign Up</h1>
+            <SignupForm />
+        </div>
+    );
+};
+
+export default SignUp;
